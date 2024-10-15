442e1b08796944a161236252e86a09bb29fa86cf
NEJI <mrkochetov@yandex.ru>
2024-10-15 20:09:39 +0300

    task_1

---
+++ task1/models.py
@@ -0,0 +1,20 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=3)
+    age = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(200)
+        ]
+    )
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    cost = models.DecimalField(max_digits=10, decimal_places=3)
+    size = models.DecimalField(max_digits=10, decimal_places=3)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer, related_name='games')
